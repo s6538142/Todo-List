ecb1efb31e55e07bbb75c5c6c22255bc0fdc0c35
c6538142 <c6538142@gmail.com>
2025-01-16 12:01:09 +0800

    Normalize .gitattributes line endings

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import ToggleSwitch from './components/ToggleSwitch';
 import './App.css';
 
 const App = () => {
+  // 定義狀態來存儲待辦事項列表
   const [todos, setTodos] = useState([]);
+
+  // 定義狀態來判斷是否按完成狀態排序
   const [isSortedByStatus, setIsSortedByStatus] = useState(false);
 
+  // 新增待辦事項的函數
   const addTodo = (text) => {
+    // 創建一個新的待辦事項物件，包含唯一的 id 和預設未完成狀態
     const newTodo = { id: Date.now(), text, completed: false };
+    // 將新的待辦事項添加到現有列表中
     setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
 
+  // 切換待辦事項完成狀態的函數
   const toggleComplete = (id) => {
     setTodos((prevTodos) =>
       prevTodos.map((todo) =>
@@ -22,14 +29,17 @@ const App = () => {
     );
   };
 
+  // 刪除待辦事項的函數
   const deleteTodo = (id) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
 
+  // 計算完成度並轉換成百分比
   const completedPercentage = Math.round(
     (todos.filter((todo) => todo.completed).length / todos.length) * 100 || 0
   );
 
+  // 根據是否排序的狀態來返回已排序或未排序的待辦事項列表
   const sortedTodos = isSortedByStatus
     ? [...todos].sort((a, b) => a.completed - b.completed)
     : todos;
--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -3,18 +3,19 @@ import './css/ToggleSwitch.css';
 
 const ToggleSwitch = ({ isSortedByStatus, setIsSortedByStatus }) => {
   return (
-    <div className="toggle-switch-container">
-      <label className="toggle-switch">
+    <div className="toggle-switch-container">     
+      <label className="toggle-switch">     
         <span className="toggle-text">
-        Move done things to end ?
-        </span>
+          Move done things to end ?
+        </span>       
         <input
           type="checkbox"
+          // 根據當前排序狀態設置核取方塊的選中狀態
           checked={isSortedByStatus}
+          // 當值變化時，更新排序狀態
           onChange={() => setIsSortedByStatus(!isSortedByStatus)}
         />
         <span className="slider"></span>
-
       </label>
     </div>
   );
